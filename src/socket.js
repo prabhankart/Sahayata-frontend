dee60b2decb3a070c89c4713323b5e04222653d3
Prabhankar Tiwari <prabhankart@gmail.com>
2025-08-28 20:29:38 +0530

    updates

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,8 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-// single shared socket instance
 export const socket = io(API_URL, {
-  transports: ["websocket"], // force WS for stability
+  transports: ["websocket"],
+  withCredentials: true,   // important if using cookies/session
 });
